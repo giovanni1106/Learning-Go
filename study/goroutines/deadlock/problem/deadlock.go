80c068b102e4188ac8321e75d392f8ae1e94cb45
Giovanni Giampauli <giovanni.acg@gmail.com>
2024-05-27 00:51:56 -0300

    create deadlock study

---
+++ study/goroutines/deadlock/problem/deadlock.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var mutex1, mutex2 sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Goroutine 1
+	go func() {
+		mutex1.Lock()
+		fmt.Println("Goroutine 1 pegou o Mutex 1")
+		time.Sleep(time.Second)
+		mutex2.Lock()
+		fmt.Println("Goroutine 1 pegou o Mutex 2")
+		time.Sleep(time.Second)
+
+		// Liberando os mutexes
+		mutex2.Unlock()
+		mutex1.Unlock()
+
+		wg.Done()
+	}()
+
+	// Goroutine 2
+	go func() {
+		mutex2.Lock()
+		fmt.Println("Goroutine 2 pegou o Mutex 2")
+		time.Sleep(time.Second)
+		mutex1.Lock()
+		fmt.Println("Goroutine 2 pegou o Mutex 1")
+		time.Sleep(time.Second)
+
+		// Liberando os mutexes
+		mutex1.Unlock()
+		mutex2.Unlock()
+
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
